005774a1b6928e1d5fc082bd0ceb466731aef624
Lucas Curtinaz <68716074+LucasCurtinaz@users.noreply.github.com>
2025-07-09 22:34:03 -0300

    Initial commit

---
+++ src/views/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <p className='rounded-xl p-4 bg-foreground text-primary-foreground'>Hello World!</p>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src', 'app'),
+      '@views': path.resolve(__dirname, 'src', 'views'),
+    },
+  }
+})
